e68eae44d82d4734024ebfdd5ae7e24404e5160d
tarilabs <matteo.mortari@gmail.com>
2022-11-09 10:26:00 +0100

    suggestions

--- src/test/java/org/examples/CCTransactionTest.java
+++ src/test/java/org/examples/CCTransactionTest.java
@@ -1,20 +1,31 @@
 package org.examples;
 
-import org.junit.Test;
+import javax.inject.Inject;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieRuntimeBuilder;
 import org.kie.api.runtime.KieSession;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
 public class CCTransactionTest {
 
+    @Inject
+    KieRuntimeBuilder runtimeBuilder;
+    
     @Test
     public void test() {
-
-        KieContainer kc = KieServices.Factory.get().getKieClasspathContainer();
-        KieSession ksession = kc.newKieSession("CCTransactionKB");
+        KieSession ksession = runtimeBuilder.newKieSession("CCTransactionKB");
 
         if (ksession == null){
             System.out.println("null ksession");
+            Assertions.fail("should never be null.");
+        } else {
+            System.out.println("KieSession not null!");
         }
         
     }
